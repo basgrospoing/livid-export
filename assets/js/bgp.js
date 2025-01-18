bf111a5bf9212c0ac16c69b78db50cd76c3b38dc
Thomasorus <contact@thomasorus.com>
2025-01-18 22:40:41 +0100

    Initial commit

---
+++ assets/js/bgp.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var active = document.getElementById('active');
+    if (active != null) {
+        active.classList.add("invert");
+    }
+});
+
+
+
